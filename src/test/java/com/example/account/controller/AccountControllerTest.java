050cfca1a8626e011f8886f8b9aa682885174557
HanSeul <w0w1278@naver.com>
2023-08-24 15:35:00 +0900

    계좌 생성 API 및 TEST 코드

--- src/test/java/com/example/account/controller/AccountControllerTest.java
+++ src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -1,18 +1,28 @@
 package com.example.account.controller;
 
 import com.example.account.domain.Account;
+import com.example.account.dto.AccountDto;
+import com.example.account.dto.CreateAccount;
 import com.example.account.type.AccountStatus;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.awt.*;
+import java.time.LocalDateTime;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,6 +38,33 @@ class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper; // json을 문자열로 변환해주는 object mapper
+
+    @Test
+    void successCreateAccount() throws Exception {
+        //given
+        given(accountService.createAccount(anyLong(), anyLong()))
+                .willReturn(AccountDto.builder()
+                        .userId(1L)
+                        .accountNumber("1234567890")
+                        .registeredAt(LocalDateTime.now())
+                        .unregisteredAt(LocalDateTime.now())
+                        .build());
+        //when
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        new CreateAccount.Request(1L, 100L) // 이건 자유롭게써도됨
+                )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andDo(print()); // cont roller에 들어갈 요청이 content안에 들어가야함
+    }
 
     @Test
     void successGetAccount() throws Exception {
